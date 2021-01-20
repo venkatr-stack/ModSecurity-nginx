d418b4aef5bfe35eedb63ffe4e22a67552baecda
venkatr-stack <66141655+venkatr-stack@users.noreply.github.com>
2021-01-20 19:31:16 +0000

    Update ngx_http_modsecurity_pre_access.c

--- src/ngx_http_modsecurity_pre_access.c
+++ src/ngx_http_modsecurity_pre_access.c
@@ -206,9 +206,9 @@ ngx_http_modsecurity_pre_access_handler(ngx_http_request_t *r)
 
 /* XXX: once more -- is body can be modified ?  content-length need to be adjusted ? */
 
-        old_pool = ngx_http_modsecurity_pcre_malloc_init(r->pool);
+        //old_pool = ngx_http_modsecurity_pcre_malloc_init(r->pool);
         msc_process_request_body(ctx->modsec_transaction);
-        ngx_http_modsecurity_pcre_malloc_done(old_pool);
+        //ngx_http_modsecurity_pcre_malloc_done(old_pool);
 
         ret = ngx_http_modsecurity_process_intervention(ctx->modsec_transaction, r);
 
